2a6678e005cbf3353cddf2a03b6ed7ba4cc5788e
a1david-boop <a1david@ucsd.edu>
2025-08-28 12:58:25 -0700

    Update week1_exercise2.cpp

--- week1_exercise2.cpp
+++ week1_exercise2.cpp
@@ -4,10 +4,19 @@
 //IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
+AnalogIn pot(p15);
+DigitalOut led(LED1);
+
 int main() {
     while (1) {
-        printf("Week 1 Exercise 2");
+        double value=pot;
+        int on=value*2000;
+        int off=2000-on;
 
+        led=1;
+        wait_ms(on);
+        led=0;
+        wait_ms(off);
         // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
         wait_ms(500); 
     }
